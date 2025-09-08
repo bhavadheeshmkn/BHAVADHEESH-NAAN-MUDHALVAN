b9fbe672145a46564949b809125e579a36e9cfc6
bhavadheeshmkn <bhavadeeshmkn@gmail.com>
2025-09-08 15:08:36 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("contactForm").addEventListener("submit", function (e) {
+  e.preventDefault();
+  document.getElementById("responseMsg").textContent = "Thanks for reaching out! I'll get back to you soon.";
+});
